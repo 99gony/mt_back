0a9a1bc652bc957eee91df1c9913583a9e086339
주건휘 <gony@jugeonhwis-Mac-mini.local>
2022-02-04 16:29:43 +0900

    db setting

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalFilters();
-  await app.listen(8080);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
